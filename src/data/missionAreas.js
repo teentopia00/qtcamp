3c4c69bfe0fc7a066ea9660895d90fe350225b0a
teentopia00 <peaktopia@gmail.com>
2026-06-09 15:28:32 +0900

    Separate mission area map data
    Constraint: Mission locations need external map buttons
    Confidence: high
    Scope-risk: narrow
    Directive: Keep Naver map links opening externally
    Tested: Module syntax checked locally
    Not-tested: Naver short-link availability

---
+++ src/data/missionAreas.js
@@ -0,0 +1,22 @@
+export const missionAreas = [
+  {
+    name: '교회입구',
+    activity: '여름 차 전도',
+    link: null,
+  },
+  {
+    name: '당감시장 입구',
+    activity: '시장 입구 주변 전도',
+    link: 'https://naver.me/ximZe4ta',
+  },
+  {
+    name: '당감시장 후문 국민은행 앞',
+    activity: '후문 유동인구 중심 전도',
+    link: 'https://naver.me/xVBiI9hH',
+  },
+  {
+    name: '당감 대웅사 약국 사거리 주변',
+    activity: '사거리 주변 전도',
+    link: 'https://naver.me/FJOypkH3',
+  },
+]
